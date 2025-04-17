e74b4a29100ba02a22a0687b0fc071b471eb59df
saevit <woosaevit@gmail.com>
2025-04-17 16:04:09 +0900

    Lv2.ArrayList->List로 변경/나눗셈 분모 0 예외 처리

--- src/calculator2/App.java
+++ src/calculator2/App.java
@@ -1,13 +1,30 @@
 package calculator2;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Calculator calculator = new Calculator();
 
-        System.out.println(calculator.add(5, 3));
-        System.out.println(calculator.sub(5, 3));
-        System.out.println(calculator.mul(5, 3));
-        System.out.println(calculator.div(5, 3));
-        System.out.println(calculator.history);
+        while (true) {
+            try {
+                // 양의 정수(0 포함)를 입력받기
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                int num1 = scanner.nextInt();
+
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                int num2 = scanner.nextInt();
+
+                int result = calculator.div(num1, num2);
+
+                System.out.println(result);
+
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+
+            System.out.println("확인");
+        }
     }
 }
--- src/calculator2/Calculator.java
+++ src/calculator2/Calculator.java
@@ -1,11 +1,12 @@
 package calculator2;
 
+import java.util.List;
 import java.util.ArrayList;
 
 public class Calculator {
     // 속성
     // 연산 결과를 저장하는 컬렉션 타입 필드
-    ArrayList<Integer> history;
+    List<Integer> history;
 
     // 생성자
     Calculator() {
@@ -31,9 +32,14 @@ public class Calculator {
         return result;
     }
 
-    public int div(int num1, int num2) {
-        int result = num1 / num2;
-        history.add(result);
-        return result;
+    public int div(int num1, int num2) throws Exception {
+        if (num2 == 0) {
+            throw new Exception("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+        }
+        else {
+            int result = num1 / num2;
+            history.add(result);
+            return result;
+        }
     }
 }
